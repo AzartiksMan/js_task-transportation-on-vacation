e26d055a406239e486545c723dfc54e749b76092
Denis Kasatskiy <azartofgod@gmail.com>
2025-04-22 15:34:02 +0300

    change solution again

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -12,12 +12,12 @@ const LONG_TERM = 7;
 const LONG_TERM_OFF = 50;
 
 function calculateRentalCost(days) {
-  if (days >= SHORT_TERM && days < LONG_TERM) {
-    return RENT_PER_DAY * days - SHORT_TERM_OFF;
-  }
-
   if (days >= LONG_TERM) {
     return RENT_PER_DAY * days - LONG_TERM_OFF;
+  }
+
+  if (days >= SHORT_TERM) {
+    return RENT_PER_DAY * days - SHORT_TERM_OFF;
   }
 
   return RENT_PER_DAY * days;
